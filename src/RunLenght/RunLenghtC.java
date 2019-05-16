b633f053bab299859ceb80ea6426426be28d378b
estani <estanipp@gmail.com>
2019-05-16 16:04:37 -0300

    RLC 1.0 y huffman andando

--- src/RunLenght/RunLenghtC.java
+++ src/RunLenght/RunLenghtC.java
@@ -1,29 +1,32 @@
 package RunLenght;
 
+import java.util.ArrayList;
+
 import Others.ImageParser;
 
 public class RunLenghtC {
 	
-	public static StringBuilder getRLC(ImageParser imagen) {
-		StringBuilder resultado = new StringBuilder();
-		Integer cont=1;
-		Integer actual = imagen.getRGB(0, 0).getRed();
+	public static Byte[] getRLC(ImageParser imagen) {
+		ArrayList<Byte> resultado = new ArrayList<Byte>();
+		int cont=1;
+		int actual = imagen.getRGB(0, 0).getRed();
 		for(int i=0;i<imagen.blockHeight(0, 0);i++) {
 			for(int j=0; j<imagen.blockWidth(0, 0);j++) {
 				if(imagen.getRGB(j, i).getRed()==actual) {
 					if(cont==255) {
-						resultado.append(actual+cont);
+						resultado.add((byte)actual);
+						resultado.add((byte)cont);
 						cont = 1;
 					}else {
 						cont++;
 					}
 				}else {
-					resultado.append(actual+cont);
-					actual = imagen.getRGB(j, i).getRed();
+					resultado.add((byte)actual);
+					resultado.add((byte)cont);
 					cont=1;
 				}
 			}
 		}
-		return resultado;
+		return (Byte[]) resultado.toArray();
 	}
 }
