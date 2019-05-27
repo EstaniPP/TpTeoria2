b7183154a7eb067635297f1ced65dccdae516981
Estanislao Perez Pena <estanipp@gmail.com>
2019-05-27 09:36:25 -0300

    Nada

--- src/Forms/gui.java
+++ src/Forms/gui.java
@@ -24,16 +24,12 @@ public class gui extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					gui frame = new gui();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+			try {
+				gui frame = new gui();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		});
 	}
 
 	/**
